177f10ba67e91485e58e8dfba5c7f109b1bf1511
asopenag <alex.sopena.91@gmail.com>
2019-07-23 19:58:17 +0200

    added color to events

--- src/js/hmodb/demoInfoJson.js
+++ src/js/hmodb/demoInfoJson.js
@@ -7,196 +7,198 @@ Queries could be based on
 */
   {
     location: {
-      city: "Pamplona",
-      country: "Spain",
-      postcode: "31003",
-      region: "Navarra",
-      street: "Francisco Bergamin Kalea, 17. Plaza de la Cruz",
+      city: 'Pamplona',
+      country: 'Spain',
+      postcode: '31003',
+      region: 'Navarra',
+      street: 'Francisco Bergamin Kalea, 17. Plaza de la Cruz',
       formatted_address:
-        "Calle de Francisco Bergamin Kalea, 17, 31003 Pamplona, Navarra",
-      google_maps_id: "UREBGQgkJMymaV8R8",
-      latitude: "42.8116805499732",
-      longitude: "-1.64030424472583",
-      distance: "0.163954353899143",
+        'Calle de Francisco Bergamin Kalea, 17, 31003 Pamplona, Navarra',
+      google_maps_id: 'UREBGQgkJMymaV8R8',
+      latitude: '42.8116805499732',
+      longitude: '-1.64030424472583',
+      distance: '0.163954353899143',
       directions: null
     },
     contact: {
-      email: "sanmiguelpamplona@gmail.com",
-      priest: "Oroz Arraiza, Luis María",
-      phone_number: "(34) 948.243.518",
-      url: "http://www.parroquiasanmiguel.com",
+      email: 'sanmiguelpamplona@gmail.com',
+      priest: 'Oroz Arraiza, Luis María',
+      phone_number: '(34) 948.243.518',
+      url: 'http://www.parroquiasanmiguel.com',
       mass_times_url:
-        "https://www.parroquiasanmiguel.com/nuestra-parroquia/horario-de-misas-en-pamplona/",
+        'https://www.parroquiasanmiguel.com/nuestra-parroquia/horario-de-misas-en-pamplona/',
       confession_times_url:
-        "https://www.parroquiasanmiguel.com/celebracion-de-los-sacramentos/confesion/",
-      last_update: [1561805184, "admin|user|..."]
+        'https://www.parroquiasanmiguel.com/celebracion-de-los-sacramentos/confesion/',
+      last_update: [1561805184, 'admin|user|...']
     },
     about: {
       church_types: [
-        "Parish",
-        "Cathedral",
-        "Shrine",
-        "Basilica",
-        "Chaplaincy",
-        "..."
+        'Parish',
+        'Cathedral',
+        'Shrine',
+        'Basilica',
+        'Chaplaincy',
+        '...'
       ], //one church may be simultaneously a basilica, a cathedral, and a shrine. http://catholicstraightanswers.com/what-is-the-difference-between-a-basilica-a-cathedral-and-a-shrine/
-      diocese: "Pamplona y Tudela",
-      diocese_type: "Archdiocese",
-      name: "San Miguel",
-      rite_type: "Roman-Latin",
-      id: "113933",
-      languages: ["Spanish", "Basque"],
+      diocese: 'Pamplona y Tudela',
+      diocese_type: 'Archdiocese',
+      name: 'San Miguel',
+      rite_type: 'Roman-Latin',
+      id: '113933',
+      languages: ['Spanish', 'Basque'],
       feast_calendar_id: 15,
-      comments: "",
-      last_update: [1561805184, ""],
+      comments: '',
+      last_update: [1561805184, ''],
       wheel_chair_access: null
     },
     events: [
       {
-        id: "Gxtupz834",
-        type: "Mass", //Mass|Confessions|Adoration|Open|Praise&Worship|Rosary|Marriage Prep|Talk|...
+        id: 'Gxtupz834',
+        color: '#4caf50', //for calendar display and visual association.
+        type: 'Mass', //Mass|Confessions|Adoration|Open|Praise&Worship|Rosary|Marriage Prep|Talk|...
         tags: [
-          "Youth",
-          "Sign language",
-          "Tridentine rite",
-          "German",
-          "Side Chapel",
-          "Confession available during event (dudo de esta tag)"
+          'Youth',
+          'Sign language',
+          'Tridentine rite',
+          'German',
+          'Side Chapel',
+          'Confession available during event (dudo de esta tag)'
         ],
-        comment: "Confessions queue closes at 12.30pm",
-        url: "", // link with more info about the event
-        time_start: "7:45",
-        time_end: "8:15",
+        comment: 'Confessions queue closes at 12.30pm',
+        url: '', // link with more info about the event
+        time_start: '7:45',
+        time_end: '8:15',
         duration: 90,
-        weekday: "123456sSfFp", //1-Monday,...,6-satuday morning, s-sunday vigil, S-Sunday, f-feast vigil, F-Feast,p-publicholiday
+        weekday: '123456sSfFp', //1-Monday,...,6-satuday morning, s-sunday vigil, S-Sunday, f-feast vigil, F-Feast,p-publicholiday
         advanced: [
           {
-            type: "onlyif|exclude|move|cancel",
-            monthday: "1,2,...,30,31", // list of comma separated values, null to include all
-            month: "1,2,...,11,12", // list of comma separated values, null to include all
-            monthweek: "1,2,3,4,5,-1,-2,-3,-4,-5",
-            weekday: "123456sSfFp", // null to include all
-            date_range: ["xxxx-06-15", "xxxx-09-10"], // summer holidays (eg 15th June to 10th September)
-            date_list: ["2019-06-15", "2019-05-18", "..."], // list of dates, null to include all
+            type: 'onlyif|exclude|move|cancel',
+            monthday: '1,2,...,30,31', // list of comma separated values, null to include all
+            month: '1,2,...,11,12', // list of comma separated values, null to include all
+            monthweek: '1,2,3,4,5,-1,-2,-3,-4,-5',
+            weekday: '123456sSfFp', // null to include all
+            date_range: ['xxxx-06-15', 'xxxx-09-10'], // summer holidays (eg 15th June to 10th September)
+            date_list: ['2019-06-15', '2019-05-18', '...'], // list of dates, null to include all
             // note this way alphabetical and calendar order coincide, allowing direct string comparisons "2019-05-08">"2019-08-16"
-            description: "sample item"
+            description: 'sample item'
           },
           {
             // exclude summer holidays (eg 15th June to 10th September)
-            type: "exclude",
-            date_range: ["xxxx-06-15", "xxxx-09-10"],
+            type: 'exclude',
+            date_range: ['xxxx-06-15', 'xxxx-09-10'],
             description:
-              "Exclude summer holidays (eg 15th June to 10th September)"
+              'Exclude summer holidays (eg 15th June to 10th September)'
           },
           {
             // event only happens on the last Thursday of the month, on even months
-            type: "onlyif",
-            weekday: "4",
-            month: "2,4,6,8,10,12",
-            monthweek: "-1",
+            type: 'onlyif',
+            weekday: '4',
+            month: '2,4,6,8,10,12',
+            monthweek: '-1',
             description:
-              "Event only happens on the last Thursday of the month, on even months"
+              'Event only happens on the last Thursday of the month, on even months'
           },
           {
             // event does not occur when it falls on the 7th July/August, and it is Friday or vigil day (sf)
-            type: "exclude",
-            weekday: "6sf",
-            month: "7",
-            monthday: "7,8",
+            type: 'exclude',
+            weekday: '6sf',
+            month: '7',
+            monthday: '7,8',
             description:
-              "Event does not occur when it falls on the 7th July/August, and it is Friday or vigil day (sf)"
+              'Event does not occur when it falls on the 7th July/August, and it is Friday or vigil day (sf)'
           },
           {
             // event is cancelled on the 13th and 5th September 2019
-            type: "cancel",
-            date_list: ["2019-09-13", "2019-09-15"],
-            description: "event is cancelled on the 13th and 5th September 2019"
+            type: 'cancel',
+            date_list: ['2019-09-13', '2019-09-15'],
+            description: 'event is cancelled on the 13th and 5th September 2019'
           },
           {
             // event is moved to the previous day if it falls on a sunday or feast day
-            type: "move",
+            type: 'move',
             move: -1440, //60*24
-            weekday: "SF",
+            weekday: 'SF',
             description:
-              "Event is moved to the previous day if it falls on a sunday or feast day"
+              'Event is moved to the previous day if it falls on a sunday or feast day'
           }
         ],
 
-        last_update: [1561805184, "admin|user|..."]
+        last_update: [1561805184, 'admin|user|...']
       },
       {
-        id: "Yuudgj734",
-        type: "Mass",
+        id: 'Yuudgj734',
+        color: '#e91e63', //for calendar display and visual association.
+        type: 'Mass',
         tags: [
-          "Youth",
-          "Sign language",
-          "Tridentine rite",
-          "German",
-          "Side Chapel",
-          "Confession available during event (dudo de esta tag)"
+          'Youth',
+          'Sign language',
+          'Tridentine rite',
+          'German',
+          'Side Chapel',
+          'Confession available during event (dudo de esta tag)'
         ],
-        comment: "Confessions queue closes at 12.30pm",
-        url: "", // link with more info about the event
-        time_start: "19:00",
-        time_end: "20:30",
+        comment: 'Confessions queue closes at 12.30pm',
+        url: '', // link with more info about the event
+        time_start: '19:00',
+        time_end: '20:30',
         duration: 30,
-        weekday: "123456sSfFp", //1-Monday,...,6-satuday morning, s-sunday vigil, S-Sunday, f-feast vigil, F-Feast,p-publicholiday
+        weekday: '123456sSfFp', //1-Monday,...,6-satuday morning, s-sunday vigil, S-Sunday, f-feast vigil, F-Feast,p-publicholiday
         advanced: [
           {
-            type: "onlyif|exclude|move|cancel",
-            monthday: "1,2,...,30,31", // list of comma separated values, null to include all
-            month: "1,2,...,11,12", // list of comma separated values, null to include all
-            monthweek: "1,2,3,4,5,-1,-2,-3,-4,-5",
-            weekday: "123456sSfFp", // null to include all
-            date_range: ["xxxx-06-15", "xxxx-09-10"], // summer holidays (eg 15th June to 10th September)
-            date_list: ["2019-06-15", "2019-05-18", "..."], // list of dates, null to include all
+            type: 'onlyif|exclude|move|cancel',
+            monthday: '1,2,...,30,31', // list of comma separated values, null to include all
+            month: '1,2,...,11,12', // list of comma separated values, null to include all
+            monthweek: '1,2,3,4,5,-1,-2,-3,-4,-5',
+            weekday: '123456sSfFp', // null to include all
+            date_range: ['xxxx-06-15', 'xxxx-09-10'], // summer holidays (eg 15th June to 10th September)
+            date_list: ['2019-06-15', '2019-05-18', '...'], // list of dates, null to include all
             // note this way alphabetical and calendar order coincide, allowing direct string comparisons "2019-05-08">"2019-08-16"
-            description: "sample item 2"
+            description: 'sample item 2'
           },
           {
             // exclude summer holidays (eg 15th June to 10th September)
-            type: "exclude",
-            date_range: ["xxxx-06-15", "xxxx-09-10"],
+            type: 'exclude',
+            date_range: ['xxxx-06-15', 'xxxx-09-10'],
             description:
-              "exclude summer holidays (eg 15th June to 10th September)"
+              'exclude summer holidays (eg 15th June to 10th September)'
           },
           {
             // event only happens on the last Thursday of the month, on even months
-            type: "onlyif",
-            weekday: "4",
-            month: "2,4,6,8,10,12",
-            monthweek: "-1",
+            type: 'onlyif',
+            weekday: '4',
+            month: '2,4,6,8,10,12',
+            monthweek: '-1',
             description:
-              "event only happens on the last Thursday of the month, on even months"
+              'event only happens on the last Thursday of the month, on even months'
           },
           {
             // event does not occur when it falls on the 7th July/August, and it is Friday or vigil day (sf)
-            type: "exclude",
-            weekday: "6sf",
-            month: "7",
-            monthday: "7,8",
+            type: 'exclude',
+            weekday: '6sf',
+            month: '7',
+            monthday: '7,8',
             description:
-              "event does not occur when it falls on the 7th July/August, and it is Friday or vigil day (sf)"
+              'event does not occur when it falls on the 7th July/August, and it is Friday or vigil day (sf)'
           },
           {
             // event is cancelled on the 13th and 5th September 2019
-            type: "cancel",
-            date_list: ["2019-09-13", "2019-09-15"],
-            description: "event is cancelled on the 13th and 5th September 2019"
+            type: 'cancel',
+            date_list: ['2019-09-13', '2019-09-15'],
+            description: 'event is cancelled on the 13th and 5th September 2019'
           },
           {
             // event is moved to the previous day if it falls on a sunday or feast day
-            type: "move",
+            type: 'move',
             move: -1440, //60*24
-            weekday: "SF",
+            weekday: 'SF',
             description:
-              "event is moved to the previous day if it falls on a sunday or feast day"
+              'event is moved to the previous day if it falls on a sunday or feast day'
           }
         ],
 
-        last_update: [1561805234, "admin|user|..."]
+        last_update: [1561805234, 'admin|user|...']
       }
     ]
-  };
+  }
 
-export default jsonDemo;
+export default jsonDemo
