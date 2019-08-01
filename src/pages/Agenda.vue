cfd80a01b0614ee8d3c2e9bb05800a7b3f48a422
asopenag <sopena@amazon.es>
2019-08-01 19:36:52 +0200

    minor testing

---
+++ src/pages/Agenda.vue
@@ -0,0 +1,32 @@
+<template>
+  <f7-page>
+    <f7-navbar title="Agenda">
+      <f7-link back>Cancel</f7-link>
+    </f7-navbar>
+    <f7-block-title>Agenda</f7-block-title>
+
+    <f7-list>
+      <f7-list-item
+        v-for="(event, index) in events"
+        :title="event.time_start + ' - ' + event.type"
+        :link="'/advanced/id/' + event.id"
+        :key="index"
+        :badge="event.advanced.length"
+      ></f7-list-item>
+    </f7-list>
+  </f7-page>
+</template>
+
+<script>
+export default {
+  props: {
+    temple_id: {
+      type: String,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
